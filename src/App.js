e7b0a7e09b3f0464ab5e4b310dc0119430366281
Emily Yu <emilyyu518@gmail.com>
2018-08-15 22:40:53 -0500

    (init) adds quick temporary content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import saveTheDate from './assets/save-the-date.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="center-contents">
+        <img className="screen-width-img" src={saveTheDate} alt="save the date" />
       </div>
     );
   }
